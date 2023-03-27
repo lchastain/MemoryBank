d31cff25baee525ec788c5d6a2583e4cab5a8563
lee <dlc@doughmain.net>
2023-03-27 15:57:49 +0400

    Support script changes and new

--- script/Win/make.bat
+++ script/Win/make.bat
@@ -1,4 +1,4 @@
-pushd ..\src\main\java
+pushd ..\..\src\main\java
 "C:\Program Files\Java\jdk-16.0.2\bin\javac.exe" -classpath ".;c:\jars\*" -Xlint MemoryBank.java
 jar cvf ..\..\..\mbank.jar *.class  -C ..\resources images/ -C ..\resources icons/ -C ..\resources help/ ..\..\..\README.md
 call noclass
